e15069acbe14b4fa40a2b17ba94bff09bc8c62ce
Bruijnesteijn, Bart (ITCDEC) - KLM <bart.bruijnesteijn@klm.com>
2020-06-28 20:18:45 +0200

    Initial Commit

---
+++ Rotation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rotation
+//
+//  Created by Bart Bruijnesteijn on 28/06/2020.
+//  Copyright © 2020 Bart Bruijnesteijn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
